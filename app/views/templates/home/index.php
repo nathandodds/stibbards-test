153b4d53151725d35c3fccf01e70cd059f073733
nathandodds <nathan@stormcreative.co.uk>
2014-01-20 17:53:18 +0000

    adding content to homepage-layout still needs changing

--- app/views/templates/home/index.php
+++ app/views/templates/home/index.php
@@ -1,2 +1,48 @@
-<header>
-</header>
+<section class="content container">
+	<article class="top-text">
+		<p class="top-text__top">We aim to provide you with a complete and comprehensive range of services to allow you to say goodbye to your loved one and reflect the life they have lived. Not only can we arrange the more traditional aspects such as funeral transport and venues, we also offer a vast array of services to make the farewell personal – including floral and musical tributes and the releasing of doves or butterflies.</p>
+		<p class="top-text__bottom">These pages have designed to take you through all the different services available. Within these pages you will find all the options and information to choose the service for your loved one.</p>
+	</article>
+</section>
+<div class="grid container">
+	<section class="grid__third">
+		<article class="home-content">
+			<img src="<?php echo DIRECTORY; ?>assets/images/services-image.png" class="content-image" />
+			<h2>Services</h2>
+			<p>We can help arrange every aspect of your loved one’s funeral – from the essential details all the way through to unique, personal tributes.</p>
+			<a href="#">View all our services</a>
+		</article>
+		<article class="home-content">
+			<img src="<?php echo DIRECTORY; ?>assets/images/brochure-image.png" class="content-image" />
+			<h2>Our brochure</h2>
+			<p>Our brochure will help guide you through the process of arranging a funeral, step-by-step.</p>
+		</article>
+	</section>
+	<section class="grid__third">
+		<article class="home-content">
+			<img src="<?php echo DIRECTORY; ?>assets/images/wtdn-image.png" class="content-image" />
+			<h2>What to do next</h2>
+			<p>It can be difficult to know what to do next following the death of a loved one. Here, we’ll guide you through the steps you need to take to register a death.</p>
+			<a href="#">Find out more</a>
+		</article>
+		<article class="home-content">
+			<img src="<?php echo DIRECTORY; ?>assets/images/plans.png" class="content-image" />
+			<h2>Funeral Plans</h2>
+			<p>Give yourself and your family peace of mind with a funeral plan that guarantees our services.</p>
+		</article>
+	</section>
+	<section class="grid__third">
+		<article class="home-content">
+			<img src="<?php echo DIRECTORY; ?>assets/images/resources-image.png" class="content-image" />
+			<h2>Resources</h2>
+			<p>We provide a range of carefully selected material including hymns, music and sermons that you can use for free when planning the service.</p>
+			<a href="#">View all our resources</a>
+		</article>		
+		<article class="home-content">
+			<img src="<?php echo DIRECTORY; ?>assets/images/contact.png" class="content-image" />
+			<h2>Contact us</h2>
+			<p>Our staff can offer sensitive advice on all aspects of funeral arrangements.</p>
+		</article>
+	</section>
+</div>
+
